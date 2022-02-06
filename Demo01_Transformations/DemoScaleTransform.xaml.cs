ad45106627ba3821f6eb22e80be7a0a00098ddb4
Stanislav Kazakov <skazakov.workmail@gmail.com>
2022-02-06 22:45:42 +0300

    Create DemoScaleTransform user control

---
+++ Demo01_Transformations/DemoScaleTransform.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Demo01_Transformations
+{
+    public partial class DemoScaleTransform : UserControl
+    {
+        public DemoScaleTransform()
+        {
+            InitializeComponent();
+        }
+    }
+}
