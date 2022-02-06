34fdc0abab5df623d46f55653b2127e53d3a4106
Stanislav Kazakov <skazakov.workmail@gmail.com>
2022-02-06 21:38:32 +0300

    Create RangeBox user control

---
+++ Demo01_Transformations/RangeBox.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Demo01_Transformations
+{
+    public partial class RangeBox : UserControl
+    {
+        public static readonly DependencyProperty LabelContentProperty =
+            DependencyProperty.Register(
+                "LabelContent",
+                typeof(object),
+                typeof(RangeBox),
+                new PropertyMetadata(null));
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                "Value",
+                typeof(double),
+                typeof(RangeBox),
+                new PropertyMetadata(0D));
+
+        public RangeBox()
+        {
+            InitializeComponent();
+        }
+
+        public object LabelContent
+        {
+            get => GetValue(LabelContentProperty);
+            set => SetValue(LabelContentProperty, value);
+        }
+
+        public double Value
+        {
+            get => (double)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+    }
+}
