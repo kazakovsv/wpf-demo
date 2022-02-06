25465f5229ea2de00ab0837204071e0c46f28050
Stanislav Kazakov <skazakov.workmail@gmail.com>
2022-02-06 22:36:05 +0300

    Create DemoSkewTransform user control

---
+++ Demo01_Transformations/DemoSkewTransform.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Demo01_Transformations
+{
+    public partial class DemoSkewTransform : UserControl
+    {
+        public DemoSkewTransform()
+        {
+            InitializeComponent();
+        }
+    }
+}
